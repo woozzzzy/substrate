79f8ead2ed4cc97e91dc757428fa0e3159dc5f60
macbook <macbook@macbooks-MacBook-Air.local>
2021-10-22 13:05:24 +0300

    Debug

--- frame/grandpa/src/lib.rs
+++ frame/grandpa/src/lib.rs
@@ -353,10 +353,10 @@ impl<T: Config> Module<T> {
 
 	/// Set the current set of authorities, along with their respective weights.
 	fn set_grandpa_authorities(authorities: &AuthorityList) {
+		sp_std::if_std! {
+			println!("in set grandpa authorities");
+		}
 		storage::unhashed::put(
-			sp_std::if_std! {
-				println!("in set grandpa authorities");
-			}
 			GRANDPA_AUTHORITIES_KEY,
 			&VersionedAuthorityList::from(authorities),
 		);
