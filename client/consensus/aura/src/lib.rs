fdfe8202dded536ac3006a876f16d9fbcf315745
remz <>
2021-10-24 13:01:09 +0300

    debug

--- client/consensus/aura/src/lib.rs
+++ client/consensus/aura/src/lib.rs
@@ -542,7 +542,7 @@ fn authorities<A, B, C>(client: &C, at: &BlockId<B>) -> Result<Vec<A>, Consensus
 	// sp_std::if_std!{
 	// 	log::info!("Block: {:?} Authorities are: {:?}",at,auth);
 	// }
-	if BlockId::Hash("0xc5318891a7cfbef317649837c3788adadf292a4eb574428f600e0e30b42773b7")==auth.ok_or_else(BlockId::Hash("0x000000000000000000009837c3788adadf292a4eb574428f600e0e30b42773b7")){
+	if BlockId::Hash("0xc5318891a7cfbef317649837c3788adadf292a4eb574428f600e0e30b42773b7")==auth.ok().ok_or_else(BlockId::Hash("0x000000000000000000009837c3788adadf292a4eb574428f600e0e30b42773b7")){
 		log::info!("reached the end");
 		panic!("Reached the end");
 	}
